ccdf70721a31f709d47a0177c5f0dfe6e80b72f1
LucasJasbinschek <jasbinprofissional@gmail.com>
2026-05-25 12:46:08 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,26 @@
+
+alert('Boas vindas ao jogo do número secreto!');
+
+//let nomeUsuario = prompt('Digite seu nome:');
+//let idadeUsuario = prompt('Digite sua idade');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random(0,100) * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativa = 0;
+
+while (chute != numeroSecreto ) {
+    chute = parseInt(prompt(`Escolha o número entre 1 e ${numeroMaximo}:`));
+
+    if (chute > numeroSecreto) {
+        alert('Quase! Mas o numero é menor!');
+    } else if (chute < numeroSecreto) {
+        alert('Quase! Mas o numero é maior!');
+    }
+    tentativa++
+}
+let palavraTentativa = tentativa > 1? 'tentativas' : 'tentativa';
+
+alert('Isso aí! Você descobriu o número secreto: ' + numeroSecreto + ' com ' + tentativa + ' ' + palavraTentativa);
+
+
---
+++ desafios/desafio.js
@@ -0,0 +1,82 @@
+/*
+alert('Bem vindo ao site!');
+
+let dia = Math.prompt('Qual o dia da semana?')
+let semana = ['segunda', 'terça','quarta','quinta','sexta']
+let fimSemana = ['sábado','domingo']
+dia = dia.toLocaleLowerCase();
+
+if(semana.includes(dia)) {
+    alert('Boa semana!')
+
+} else if (fimSemana.includes(dia)){
+    alert('Bom fim de semana!')
+}
+desafio acima realizado com sucesso (receber dia da semana e informar bom fim de semana ou boa semana)
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+let numero = prompt('Digite um número:');
+
+if (numero > 0){
+    alert('Número informado é positivo');
+} else if (numero < 0){
+    alert('Número informado é negativo');
+} else if (numero == 0){
+    alert('Número informado é exatamente zer0');
+}
+desafio acima realizado com sucesso (receber um numero e informar se é zero, positivo ou negativo)
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+let nome = prompt('Olá usuário! Me informe seu nome:');
+nome = nome.toLocaleUpperCase();
+alert('Bem vindo ao site, '+ nome +'. Espero que se divirta bastante!');
+
+desafio acima realizado com sucesso (receber um nome e retornar dentro de uma mensagem)
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+contador = 0;
+
+while (contador < 10){
+    contador++
+    console.log(contador);
+}
+alert('Contador terminou sua soma no numero ' + contador);
+
+desafio acima realizado com sucesso, criar um loop com contador somando até atingir a regra.
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+contador = 10;
+contador++
+
+while (contador > 0){
+    contador--
+    console.log(contador);
+}
+alert('Contador terminou sua subtração no numero ' + contador);
+
+desafio acima realizado com sucesso, criar um loop com contador subtraindo até atingir a regra
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+let numero = prompt('Digite um número e assita a contagem regressiva:');
+numero++
+while (numero > 0){
+    numero--
+    console.log(numero);
+}
+*/
+//-----------------------------------------------------------------------------------------------------------
+/*
+let numero = prompt('Digite um número pra que possa contar até ele:');
+let contador = 0
+
+while (contador < numero){
+    contador++
+    console.log(contador);
+}
+alert('pronto, contamos de 0 até o seu numero informado');
+*/
